6470da8aac4d8f2e004e19b36408ba699fe7958d
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-23 19:25:42 +0000

    finished indirect association

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,9 @@ class User < ApplicationRecord
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name "FollowRequest"
   has_many :received_follow_requets, foreign_key: :recipient_id, class_name "FollowRequest"
   has_many :own_photos, foreign_key: owner_id, class_name: "Photo"
+  has_many :liked_photos, through: :likes, source: :photos
+  has_many :leaders, through: :sent_follow_requests, source: :recipient
+  has_many :followers, through: :accepted_received_follow_requests, source: :sender
+  has_many :feed, through: :leaders, source: :own_photos
+  has_many :discover, through: :leaders, source: :liked_photos
 end
