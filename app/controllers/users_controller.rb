225716d089bfcbfb9e400f782e30ae1233dac792
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-25 16:50:34 +0000

    added feed route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:username))
   end
 
+  def liked
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
 end
